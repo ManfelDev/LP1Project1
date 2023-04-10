837ade56adf239392e3ba2255b5d2cbe4e309c16
Rafael José <ra.david@hotmail.com>
2023-04-10 21:07:40 +0100

    Add project 'LampsPuzzle'

---
+++ LampsPuzzle/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LampsPuzzle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
